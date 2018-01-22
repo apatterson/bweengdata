ef4c4e09c96cb63f78cd90fd959daec565a74061
apatterson <anthony.patterson2.0@gmail.com>
2018-01-22 08:13:08 +0000

    add personal stats

--- main.py
+++ main.py
@@ -14,10 +14,11 @@
 
 import logging
 # [START app]
-from bokeh.document import Document
+from bokeh.models import ColumnDataSource, LabelSet, FixedTicker, Grid
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.themes import Theme
+from bokeh.io import curdoc
 
 from flask import Flask, request, render_template
 from stravalib.client import Client
@@ -36,6 +37,7 @@ def hello(club_name='Bweeng Trail Blazers'):
         access_token = client.exchange_code_for_token(client_id=22031, client_secret='', code=code)
 
         clubs = client.get_athlete_clubs()
+        me = client.get_athlete()
         if len(clubs) <1:
             return 'You are not a member of any Strava clubs'
         try:
@@ -43,26 +45,47 @@ def hello(club_name='Bweeng Trail Blazers'):
         except StopIteration:
             return '<ul>' + ''.join('<li><a href="/' + c.name + '?code=' + code +
                                     '">' + c.name + '</a></li>' for c in clubs) + '</ul>'
+        source = ColumnDataSource(data=dict(speed=[4, 4, 4, 4, 4, 4, 3.333, 2.777, 3.83],
+                                            distance=[5000, 10000, 8000, 16000, 21097, 42195, 1000, 1000, 1000],
+                                            names=['5K', '10K', '5 Miles', '10 Miles',
+                                                       'Half', 'Marathon', '5 mins/K', '6 mins/K', '7 mins/Mile']))
+        labels = LabelSet(x='distance', y='speed', text='names', level='glyph',
+                            source=source, render_mode='canvas')
+        plot = figure()
 
-        plot = figure(x_axis_label='Distance (metres)', y_axis_label='Average Speed (m/s)',
-                      background_fill_color="rgb(255,87,34)", sizing_mode="scale_width",
-                      outline_line_color="white")
         for activity in myclub.activities:
+            radius = 300
+            if activity.athlete.id == me.id:
+                radius = 500
             if activity.type == 'Run':
-                if activity.workout_type != '1':
+                if activity.workout_type != '1' and activity.distance.num > 1000:
                     plot.circle(activity.distance.num, activity.average_speed.num, color="white", alpha=0.5,
-                                radius=300)
+                                size=30)
         for activity in myclub.activities:
             if activity.type == 'Run':
-                if activity.workout_type == '1':
+                if activity.workout_type == '1' and activity.distance.num > 1000:
                     plot.circle(activity.distance.num, activity.average_speed.num, color='red', legend='Race',
-                                alpha=0.5, radius=300)
+                                alpha=0.5, size=30)
+        for activity in myclub.activities:
+            if activity.type == 'Run':
+                if activity.distance.num > 1000:
+                    if activity.athlete.id == me.id:
+                        plot.triangle(activity.distance.num, activity.average_speed.num, legend='Me',
+                                   size=15, color='grey', line_width=3)
+
+        plot.xgrid[0].ticker = FixedTicker(ticks=[5000, 8000, 10000, 16000, 21097, 42195])
+        plot.ygrid[0].ticker = FixedTicker(ticks=[2.777, 3.3333, 3.83])
+        plot.xgrid.band_fill_color = 'rgb(255,87,34)'
+        plot.xgrid.band_fill_alpha = 0.3
+        plot.ygrid.band_fill_color = 'rgb(255,87,34)'
+        plot.ygrid.band_fill_alpha = 0.3
+
+        plot.add_layout(labels)
 
         theme = Theme(filename="./theme.yaml")
-        doc = Document(theme=theme)
-        doc.add_root(plot)
+        curdoc().theme = theme
 
-        script, div = components(doc)
+        script, div = components(plot)
         return render_template("chart.html", script=script, plot=div, club=myclub, clubs=clubs, code=code, client=client)
     else:
         return "This app requires access to Strava. <p><a href='" + authorize_url + "'><img src=\"static/btn_strava_connectwith_orange.png\"></img> </a>"
