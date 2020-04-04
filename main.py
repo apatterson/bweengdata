2ffe32487090bb8fea0b46875285941114b067a7
Anthony Patterson <anthony.patterson2.0@gmail.com>
2020-04-04 21:21:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,29 @@ def hello(club_name='Bweeng Trail Blazers'):
 
     code = request.args.get('code')
     if code:
-        access_token = client.exchange_code_for_token(client_id=22031, client_secret=strava_client_secret, code=code)
+        # Extract the code from your webapp response
+        token_response = client.exchange_code_for_token(client_id=1234, client_secret='asdf1234', code=code)
+        access_token = token_response['access_token']
+        refresh_token = token_response['refresh_token']
+        expires_at = token_response['expires_at']
+
+        # Now store that short-lived access token somewhere (a database?)
+        client.access_token = access_token
+        # You must also store the refresh token to be used later on to obtain another valid access token 
+        # in case the current is already expired
+        client.refresh_token = refresh_token
+
+        # An access_token is only valid for 6 hours, store expires_at somewhere and
+        # check it before making an API call.
+        client.token_expires_at = expires_at
+
+        # ... time passes ...
+        if time.time() > client.token_expires_at:
+            refresh_response = client.refresh_access_token(client_id=1234, client_secret='asdf1234',
+                                                           refresh_token=client.refresh_token)
+            access_token = refresh_response['access_token']
+            refresh_token = refresh_response['refresh_token']
+            expires_at = refresh_response['expires_at']
 
         clubs = client.get_athlete_clubs()
         me = client.get_athlete()
